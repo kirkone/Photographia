8c56cb7d4b20d638f4f7b4cfebf1207e39a5675d
Thomas Müller <tmueller@cpan.org>
2021-07-09 22:49:21 +0200

    Update main.ts
    Minor changes

--- src/Deck/src/scripts/main.ts
+++ src/Deck/src/scripts/main.ts
@@ -2,6 +2,8 @@ interface SafariTouchEvent extends TouchEvent {
     scale: number;
 }
 
+const isSafariTouchEvent = (e: TouchEvent): e is SafariTouchEvent => "scale" in e; 
+
 interface WebKitStyle extends CSSStyleDeclaration {
     WebkitTransform: string;
 }
@@ -29,38 +31,34 @@ function pinchZoom(imageElement: HTMLElement): void {
             start.x = (e.touches[0].pageX + e.touches[1].pageX) / 2;
             start.y = (e.touches[0].pageY + e.touches[1].pageY) / 2;
             start.distance = distance(e);
+            
+            const articleElement = imageElement.closest('article');
+            if (articleElement) {
+                articleElement.classList.add('zooming');
+            }
         }
     });
 
     imageElement.addEventListener('touchmove', (event: TouchEvent) => {
         if (event.touches.length === 2) {
             event.preventDefault(); // Prevent page scroll
-            let scale: number;
-
+            
             // Safari provides event.scale as two fingers move on the screen
             // For other browsers just calculate the scale manually
-            if ((event as SafariTouchEvent).scale) {
-                scale = (event as SafariTouchEvent).scale;
-            } else {
-                const deltaDistance = distance(event);
-                scale = deltaDistance / start.distance;
-            }
+            const scale = isSafariTouchEvent(event) ?
+                  event.scale : distance(event) / start.distance;
 
             imageElementScale = Math.min(Math.max(1, scale), 4);
 
             // Calculate how much the fingers have moved on the X and Y axis
-            const deltaX = (((event.touches[0].pageX + event.touches[1].pageX) / 2) - start.x) * 2; // x2 for accelarated movement
-            const deltaY = (((event.touches[0].pageY + event.touches[1].pageY) / 2) - start.y) * 2; // x2 for accelarated movement
+            const [touch1, touch2] = event.touches;
+            const deltaX = (((touch1.pageX + touch2.pageX) / 2) - start.x) * 2; // x2 for accelarated movement
+            const deltaY = (((touch1.pageY + touch2.pageY) / 2) - start.y) * 2; // x2 for accelarated movement
 
             // Transform the image to make it grow and move with fingers
             imageElement.style.setProperty('--zoom-x', `${deltaX}px`);
             imageElement.style.setProperty('--zoom-y', `${deltaY}px`);
             imageElement.style.setProperty('--zoom-scale', `${imageElementScale}`);
-
-            const articleElement = imageElement.closest('article');
-            if (articleElement) {
-                articleElement.classList.add('zooming');
-            }
         }
     });
 
